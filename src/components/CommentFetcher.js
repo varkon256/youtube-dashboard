512254fe2748794fddeca4aae5021ec6af7118ba
Naseem Shah <naseemshahvarikodan@gmail.com>
2020-09-01 21:29:42 +0530

    fetch comment frim YT API

--- src/components/CommentFetcher.js
+++ src/components/CommentFetcher.js
@@ -1,11 +1,64 @@
 import React from 'react';
 
+import {useState, useEffect} from 'react';
+import '../App.css';
+import CommentBox from './CommentBox';
 function CommentFetcher() {
-  return (
-    <div>
-        comment fetcher
-    </div>
-  );
+    const [error, setError] = useState(null);
+    const [isLoaded, setIsLoaded] = useState(false);
+    const [maxResult, setMaxResult] = useState (1);
+    const [comments, setComments] = useState([]);
+
+    function handleFetch(max) {
+
+        
+    }
+    useEffect(() => {
+        fetch(`https://www.googleapis.com/youtube/v3/commentThreads?part=snippet&maxResults=10&order=relevance&videoId=_VB39Jo8mAQ&key=AIzaSyCapt-WkmhFfuvLDDlfoMk86hBsaAesLrI`)
+        .then(res => res.json())
+        .then(
+            (result) => {
+                setIsLoaded(true);
+                setComments(result.items);
+            },
+            (error) => {
+                setIsLoaded(true);
+                setError(error);
+            }
+        )
+    }, [])
+        return (
+            <div className="commentBoxContainer">
+                <div className="form">
+                    <input type="text" placeholder="video link"></input>
+                    <input type="number" min="0" placeholder="no. results"></input>
+                    <button onClick={handleFetch(maxResult)} >Fetch</button>        
+                </div>
+                <div>
+                    {
+                    
+                    comments.map(comment => ( 
+                        <div>
+                            {console.log(comments)}                                  
+                            <CommentBox 
+                                date = {comment.snippet.topLevelComment.snippet.publishedAt}
+                                author = {comment.snippet.topLevelComment.snippet.authorDisplayName }
+                                dpUrl = {comment.snippet.topLevelComment.snippet.authorProfileImageUrl }
+                                comment = {comment.snippet.topLevelComment.snippet.textOriginal}
+                                likes = {comment.snippet.topLevelComment.snippet.likeCount}
+                                replies = {comment.snippet.totalReplyCount}
+
+                            />                    
+                        </div> 
+                    ))
+                    }                    
+                </div>
+            </div>
+        );
 }
+
+
+
+
 
 export default CommentFetcher;
