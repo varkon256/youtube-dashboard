9abbe49e6140a0e397bab0618b55f403a8f1d375
varkon256 <kondavarsha@hotmail.com>
2020-09-02 19:13:32 -0700

    Changes to question classification script

--- python_scripts/comment_question.py
+++ python_scripts/comment_question.py
@@ -1,9 +1,3 @@
-#!/usr/bin/env python
-# coding: utf-8
-
-# In[15]:
-
-
 import pandas as pd
 import nltk
 from sklearn.feature_extraction.text import TfidfVectorizer
@@ -11,17 +5,11 @@ from sklearn.ensemble import GradientBoostingClassifier
 from sklearn.metrics import classification_report
 
 
-# In[16]:
+# List of comments
+comments = []
 
 
-file = open('comments.txt', 'r')
-comments_list = file.readlines()
-comments = pd.DataFrame(comments_list, columns=["Comment"])
-
-
-# In[17]:
-
-
+# https://stackoverflow.com/questions/49100615/nltk-detecting-whether-a-sentence-is-interogative-or-not
 nltk.download('nps_chat')
 posts = nltk.corpus.nps_chat.xml_posts()
 posts_text = [post.text for post in posts]
@@ -41,20 +29,11 @@ y_test = y[int(len(posts_text)*0.2):]
 gb = GradientBoostingClassifier(n_estimators = 400, random_state=0)
 gb.fit(X_train, y_train)
 
-
-# In[23]:
-
-
 question_comments = []
-for index, row in comments.iterrows():
-    type_of_comment = gb.predict(vectorizer.transform([row['Comment']]))
-    if (type_of_comment == 'ynQuestion' or type_of_comment == 'whQuestion' or '?' in row['Comment']):
-        question_comments.append(row['Comment'])
+for comment in comments:
+    type_of_comment = gb.predict(vectorizer.transform([comment]))
+    if (type_of_comment == 'ynQuestion' or type_of_comment == 'whQuestion' or '?' in comment):
+        question_comments.append(comment)
 question_comments
-
-
-# In[ ]:
-
-
 
 
