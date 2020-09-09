f47e3f6e1a071d97e94956cda8550a8033eea9b6
varkon256 <kondavarsha@hotmail.com>
2020-09-08 18:40:29 -0700

    include build folder

---
+++ build/precache-manifest.2823bc91ce5435015a3956a0836ea2e4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9364f77e7e73c4c797da8c922500f8a3",
+    "url": "/index.html"
+  },
+  {
+    "revision": "78f0491449cd3e990089",
+    "url": "/static/css/main.1bc5609c.chunk.css"
+  },
+  {
+    "revision": "5df62e9bf2b49c62d14d",
+    "url": "/static/js/2.6207798f.chunk.js"
+  },
+  {
+    "revision": "0e38a78b5edd830e3f8d27afa62b2282",
+    "url": "/static/js/2.6207798f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "78f0491449cd3e990089",
+    "url": "/static/js/main.6b19f9cd.chunk.js"
+  },
+  {
+    "revision": "4df8cce27060e624fa68",
+    "url": "/static/js/runtime-main.3ffa9621.js"
+  }
+]);
